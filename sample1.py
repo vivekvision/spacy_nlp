85001a28ca1f2b6fea6d94dfb43cb5ce7944b64a
Vivek Kumar <vivek.vision@gmail.com>
2023-06-05 19:45:24 +0800

    Update sample1.py
    update

--- sample1.py
+++ sample1.py
@@ -1,6 +1,9 @@
 import spacy
 import pandas as pd
 
+# first load the English language model using spacy.load("en_core_web_sm"). Then define a function extract_country_names 
+# that takes a text input, processes it with spaCy, and extracts the named entities labeled as geopolitical entities (GPE).
+
 # Load the English language model in spaCy
 nlp = spacy.load("en_core_web_sm")
 
@@ -8,6 +11,7 @@ nlp = spacy.load("en_core_web_sm")
 df = pd.DataFrame({'text': ['I love visiting France and Italy.', 'Germany is a beautiful country.', 'Spain has amazing food.']})
 
 # Function to extract country names from text using spaCy
+# it returns a list with country names
 def extract_country_names(text):
     doc = nlp(text)
     country_names = []
@@ -15,6 +19,8 @@ def extract_country_names(text):
         if ent.label_ == 'GPE':  # Filter entities labeled as geopolitical entities (countries, cities, etc.)
             country_names.append(ent.text)
     return country_names
+
+# use the apply function along with a lambda function to apply the extract_country_names function to each element of the 'text' column.
 
 # Apply the function to the 'text' column and count the country names
 df['country_count'] = df['text'].apply(lambda x: len(extract_country_names(x)))
